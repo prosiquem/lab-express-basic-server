046c5a3b42e801651ea65105da273f397666b361
Pedro Rosique <rosiquemurciapedro@gmail.com>
2024-11-25 16:01:51 +0100

    server with express and tested with postman

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require('express')
+const logger = require('morgan')
+const app = express()
 
+app.use(logger('dev'))
+app.use(express.json())
 
+const projects = require('./data/projects.json')
+const articles = require('./data/articles.json')
 
-// CREATE EXPRESS APP
-// Here you should create your Express app:
+app.get('/api/projects', (req, res) => {
 
+    res.json(projects)
+})
 
+app.get('/api/articles', (req, res) => {
 
-// MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
+    res.json(articles)
+})
 
+app.get('*', (req, res) => {
+    res.status(404).send('Página no encontrada')
+})
 
-
-// ROUTES
-// Start defining your routes here:
-
-
-
-// START THE SERVER
-// Make your Express server listen on port 5005:
+app.listen(5005, console.log('Server runing on port 5005'))
